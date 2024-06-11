e171a05ed2d33efef2e50ab548c99544a6688b6f
sdyproject <ehddlrhome@naver.com>
2024-06-11 23:42:51 +0900

    Jwt-access+refresh

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.demo.config;
 
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,11 +14,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.example.demo.filter.JWTFilter;
 import com.example.demo.filter.LoginFilter;
 import com.example.demo.utils.JwtUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -40,6 +46,30 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
+		http
+			.cors((cors) -> cors
+					.configurationSource(new CorsConfigurationSource() {
+						
+						@Override
+						public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+						
+							CorsConfiguration configuration =new CorsConfiguration();
+							
+							configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+	                        configuration.setAllowedMethods(Collections.singletonList("*"));
+	                        configuration.setAllowCredentials(true);
+	                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+	                        configuration.setMaxAge(3600L);
+
+							configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+							
+							
+							
+							
+							return configuration;
+						}
+					}));
 		
 		
 		
@@ -52,7 +82,7 @@ public class SecurityConfig {
 			.httpBasic((auth) -> auth.disable());
 		http
 		  .authorizeHttpRequests((auth) -> auth
-				  .requestMatchers("/login", "/", "/join","/member","/api/demo","/calendar","/joinForm","/board/**").permitAll()
+				  .requestMatchers("/login", "/","/member").permitAll()
 						 .requestMatchers("/admin").hasRole("ADMIN") 
 				  .anyRequest().authenticated());
 		http
